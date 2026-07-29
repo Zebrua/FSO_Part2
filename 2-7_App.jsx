d7a18d395a9bb52f9c73e50e171794eba34b6769
Zebrua <141924342+Zebrua@users.noreply.github.com>
2026-07-29 17:42:28 +0300

    Add specific check for 'Arto Hellas' in addName

--- 2-7_App.jsx
+++ 2-7_App.jsx
@@ -24,7 +24,10 @@ const App = () => {
   const addName = (event) => {
     event.preventDefault()
     const copy = [...persons]
-    if (copy.find(({name}) => name === newName)) {
+    if (newName === "Arto Hellas") {
+      alert(`Arto Hellass is already added to phonebook.`)
+    }
+    else if (copy.find(({name}) => name === newName)) {
       alert(`${newName} is already added to phonebook.`)
     }
     else{
