7c847fcb035dfdc72fd9c84d73868c20decc584d
Zebrua <141924342+Zebrua@users.noreply.github.com>
2026-07-30 20:53:50 +0300

    Add notification banner on update and delete

--- 2-16_App.jsx
+++ 2-16_App.jsx
@@ -1,38 +1,11 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
+import phonebook_service from "./services/phonebook"
+import Notification from "./components/Notification"
 
-const Notification = ({ message }) => {
-  if (message === null) {
-    return null
-  }
-  return ( 
-    <div className='confirmation'>
-      {message}
-    </div>
-  )
-}
-const createNew = (data) => {
-  const request = axios.post('http://localhost:3001/persons', data)
-  return request.then(response => response.data)
-}
-
-const getAll = () => {
-  const request = axios.get('http://localhost:3001/persons')
-  return request.then(response => response.data)
-}
-
-const removeAction = (id) => {
-  const request = axios.delete(`http://localhost:3001/persons/${id}`)
-  return window.location.replace('http://localhost:5173/')
-}
-
-const updateAction = (person) => {
-  const request = axios.put(`http://localhost:3001/persons/${person.id}`, person)
-  return window.location.replace('http://localhost:5173/')
-}
 
 const Newport = (props) => {
-  const addName = (event) => {
+  const addName = async(event) => {
     event.preventDefault()
     const copy = [...props.persons]
     if (copy.find(({name}) => name === props.newName)) {
@@ -40,7 +13,13 @@ const Newport = (props) => {
       if (newcon === true){
         const Name = copy.find(({name}) => name === props.newName)
         Name.number = props.newNumber
-        updateAction(Name)
+        phonebook_service.updateAction(Name, props)
+        await props.setMessage(`Updated ${props.newName}`)
+        setTimeout(() => {
+          props.setMessage(null)
+        }, 3000)
+        await window.location.reload()
+        
       }
     }
     else{
@@ -49,11 +28,11 @@ const Newport = (props) => {
         number: props.newNumber,
         id: String(props.persons.length + 1)
       }
+      phonebook_service.createNew(Name)
       props.setMessage(`Added ${props.newName}`)
       setTimeout(() => {
         props.setMessage(null)
       }, 3000)
-      createNew(Name)
       copy.push(Name)
       props.setPersons(copy)
       props.setNewName("")
@@ -106,6 +85,14 @@ const Filtering = (props) => {
   )
 }
 const Phonebook = (props) => {
+  const removePerson = async (person) => {
+    phonebook_service.removeAction(person.id)
+    props.setMessage(`Deleted ${person.name}`)
+    setTimeout(() => {
+      props.setMessage(null)
+    }, 3000)
+    await window.location.reload()
+  }
   if (props.filter === ""){
     const list = props.persons
     return(
@@ -113,7 +100,7 @@ const Phonebook = (props) => {
         {list.map((person) => (
           <>
           <p key={person.id}>{person.name} {person.number}</p>
-          <button onClick={() => removeAction(person.id)}>Delete</button>
+          <button onClick={() => removePerson(person)}>Delete</button>
           </>
         ))}
       </div>
@@ -137,8 +124,9 @@ const App = () => {
   const [filterName, setFilterName] = useState('')
   const [message, setMessage] = useState(null)
 
+  
   useEffect(() => {
-    getAll()
+    phonebook_service.getAll()
     .then(Persons => {
       setPersons(Persons)
     })
@@ -160,7 +148,7 @@ const App = () => {
       </div>
       <h2>Numbers</h2>
       <div>
-        <Phonebook persons={persons} filter={filterName} />
+        <Phonebook persons={persons} filter={filterName} message={message} setMessage={setMessage} />
       </div>
     </div>
   )
